e74fb992509d1464c9d069ff31b134e5b6ed4e39
Vicki Yang <vickiwyang@gmail.com>
2021-10-15 15:40:35 -0500

    homepage menu items

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.menuBox}>
+        <Text style={styles.menuText}>Doctors</Text>
+      </View>
+      <View style={styles.menuBox}>
+        <Text style={styles.menuText}>Medications</Text>
+      </View>
+      <View style={styles.menuBox}>
+        <Text style={styles.menuText}>Symptoms Diary</Text>
+      </View>
+      <View style={styles.menuBox}>
+        <Text style={styles.menuText}>Timeline View</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#60AB9A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  menuBox: {
+    borderColor: '#F9F8F8',
+    borderWidth: 4,
+    width: 300,
+    padding: 20,
+    margin: 10,
+    alignItems: 'center',
+  },
+  menuText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#F9F8F8',
+    fontFamily: 'Avenir'
+  }
 });
